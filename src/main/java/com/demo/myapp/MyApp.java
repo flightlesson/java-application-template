ae20fa968d755603f776a0d0a619c50904c39a9f
Timothy E Singletary <tsingle@muddypaddock.com>
2016-02-16 10:01:09 -0500

    re-arranged cli help message parts

--- src/main/java/com/demo/myapp/MyApp.java
+++ src/main/java/com/demo/myapp/MyApp.java
@@ -12,24 +12,28 @@ import org.apache.log4j.PropertyConfigurator;
 import org.apache.log4j.xml.DOMConfigurator;
 
 /**
- * Overview of the application.
+ * Replace this with an overview of the application that answers the questions
+ * "what is this?" and "why would I want to use it?".
  */
 public class MyApp implements Runnable {
     static final Logger LOGGER = Logger.getLogger(MyApp.class.getName());
     
-    static private final String USAGE = "stuff on usage line after 'usage:'";
-    static private final String HEADER = "Brief summary of what the application does.\nOptions are:";
+    static private final String USAGE = "replace this with stuff that will appear after 'usage:'";
+    static private final String HEADER = "Replace this with a brief summary describing what the application does.\nOptions are:";
+    static private final String FOOTER = "\n"
+            +"Replace this with a longer description of the application "
+            +"that answers 'what does this do?' and 'why should I use it?'.\n"
+            +"\n"
+            +"I like to use a blank line to separate paragraphs.\n"
+            +"\n";
     static private final Options OPTIONS;
     static {
         OPTIONS = new Options();
         OPTIONS.addOption("h","help",false,"Print this message.");
 	OPTIONS.addOption("v","verbose",false,"Turn on verbose output.");
         OPTIONS.addOption(null,"l4jconfig",true,"Path to the log4j configuration file [./l4j.lcf]");
+        // Add application specific options here.
     }
-    static private final String FOOTER = "\nLonger description of the application. "
-            +"Apache Commons CLI is pretty good about splitting long paragraphs.\n"
-            +"\n"
-            +"I like to use a blank line to separate paragraphs.\n";
     
     static public void main(String[] args) {
         try {
